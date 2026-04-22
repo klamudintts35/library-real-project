f1f62bcf8df4f8162294b288a9b6d44d814098cf
klamudintts35 <mklamuddin0786@gmail.com>
2026-04-22 23:44:09 +0530

    Update bestURL.js

--- frontend/src/utils/bestURL.js
+++ frontend/src/utils/bestURL.js
@@ -2,6 +2,6 @@
 // Local me localhost:4000
 // Live hone par isi file me URL change karna hoga
 
-const bestURL = "http://localhost:4000/api";
+const bestURL = "https://library-real-project.onrender.com/api";
 
 export default bestURL;
