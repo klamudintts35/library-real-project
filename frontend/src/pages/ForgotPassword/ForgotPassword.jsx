1ca17b72dcc5083f9208e6ff6e6e2d27c4c6804f
klamudintts35 <mklamuddin0786@gmail.com>
2026-04-28 09:38:03 +0530

    Comment out OTP alert in ForgotPassword component
    Comment out alert displaying OTP for testing purposes.

--- frontend/src/pages/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -21,7 +21,7 @@ const ForgotPassword = () => {
       });
 
       if (data.success) {
-        alert(`Your OTP is: ${data.otp}`);/////ese bas otp dekhne ke liye use kar rahe hai
+        //alert(`Your OTP is: ${data.otp}`);/////ese bas otp dekhne ke liye use kar rahe hai
         localStorage.setItem("resetMobile", mobile);
         navigate("/verify-otp");
       }
